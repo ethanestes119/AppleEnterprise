ca8a55070eb75205a31687904b746a1001d3c04c
APPLEH4CKZ <42897517+APPLEH4CKZ@users.noreply.github.com>
2020-03-13 19:16:42 -0700

    Add files via upload

---
+++ Website/Login/main.js
@@ -0,0 +1,29 @@
+function clicked() {
+
+	var user = document.getElementById('username');
+	var pass = document.getElementById('password');
+
+	var adminuser = "admin","root";
+	var adminpassword = "pass123","pass222";
+
+
+	if(user.value == admin) {
+
+		if(pass.value == adminpassword) {
+
+			window.alert("You are logged in as " + user.value);
+			window.open("https://appleh4ckz.github.io/AppleEnterprise/Website/index/");
+
+		} else {
+
+			window.alert("Please try again, Username or Password was typed incorrectly.");
+
+		}
+
+	} else {
+
+			window.alert("Please try again, Username or Password was typed incorrectly.");
+
+	}
+
+}
